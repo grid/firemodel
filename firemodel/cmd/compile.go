grid/firemodel

commit a784f729933482d6a07d434a4885b251b984f474

Update compile API per #7

firemodel/cmd/compile.go
@@ -1,0 +1,75 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+	"github.com/mickeyreiss/firemodel"
+	"github.com/mickeyreiss/firemodel/internal/tempwriter"
+	"fmt"
+	"github.com/go-errors/errors"
+)
+
+var compileReq struct {
+	wipe        bool
+	langOutDirs map[string]*string
+}
+
+func init() {
+	compileCmd.PersistentFlags().StringVar(&req.schema, "schema", "schema.firemodel", "Path to firemodel schema.")
+	compileCmd.PersistentFlags().BoolVarP(&compileReq.wipe, "wipe", "f", false, "Confirms it is ok to rm -rf the output directories. (This is generally something you want, but defaults off for safety.)")
+
+	compileReq.langOutDirs = make(map[string]*string)
+	for _, modeler := range firemodel.AllModelers() {
+		compileReq.langOutDirs[modeler] = new(string)
+		compileCmd.PersistentFlags().StringVar(compileReq.langOutDirs[modeler], modeler+"_out", "", fmt.Sprintf("%s output directory", modeler))
+	}
+}
+
+var compileCmd = &cobra.Command{
+	Use:     "compile",
+	Aliases: []string{"c"},
+	Short:   "Type-safe, cross-platform models for Firestore",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		for k := range compileReq.langOutDirs {
+			if !cmd.Flag(k + "_out").Changed {
+				delete(compileReq.langOutDirs, k)
+			}
+		}
+		if len(compileReq.langOutDirs) == 0 {
+			return errors.New("no languages requested")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		r, err := os.Open(req.schema)
+		if err != nil {
+			panic(err)
+		}
+
+		schema, err := firemodel.ParseSchema(r)
+		if err != nil {
+			panic(err)
+		}
+
+		config := &firemodel.Config{
+			SourceCoderProvider: func(prefix string) firemodel.SourceCoder {
+				return tempwriter.New(prefix, compileReq.wipe)
+			},
+		}
+
+		for language, outDir := range compileReq.langOutDirs {
+			if outDir != nil {
+				continue
+			}
+			config.Languages = append(config.Languages, firemodel.Language{
+				Language: language,
+				Output:   *outDir,
+			})
+		}
+
+		if err := firemodel.Run(schema, config); err != nil {
+			panic(err)
+		}
+
+	},
+}

firemodel/cmd/firemodel.go
@@ -1,24 +1,18 @@
 package cmd
 
 import (
-	"os"
-
+	// Modeler registrations:
 	_ "github.com/mickeyreiss/firemodel/langs/go"
 	_ "github.com/mickeyreiss/firemodel/langs/ios"
 	_ "github.com/mickeyreiss/firemodel/langs/ts"
-	"path"
+
 	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/go-errors/errors"
-	"github.com/mickeyreiss/firemodel/internal/tempwriter"
-	"github.com/mickeyreiss/firemodel"
 )
 
 var (
 	req struct {
 		schema string
-		outDir string
-		wipe   bool
 	}
 )
 
@@ -27,69 +21,9 @@ var rootCmd = &cobra.Command{
 	Short: "Type-safe, cross-platform models for Firestore",
 }
 
-var compileCmd = &cobra.Command{
-	Use:     "compile",
-	Aliases: []string{"c"},
-	Short:   "Type-safe, cross-platform models for Firestore",
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-		r, err := os.Open(req.schema)
-		if err != nil {
-			panic(err)
-		}
-
-		schema, err := firemodel.ParseSchema(r)
-		if err != nil {
-			panic(err)
-		}
-
-		config := &firemodel.Config{
-			SourceCoderProvider: func(prefix string) firemodel.SourceCoder {
-				return tempwriter.New(prefix, req.wipe)
-			},
-		}
-		if len(args) == 0 {
-			args = firemodel.AllModelers()
-		}
-
-		for _, language := range args {
-			config.Languages = append(config.Languages, firemodel.Language{
-				Language: language,
-				Output:   path.Join(req.outDir, language),
-			})
-		}
-
-		if err := firemodel.Run(schema, config); err != nil {
-			panic(err)
-		}
-
-	},
-}
-
-var cleanCmd = &cobra.Command{
-	Use: "clean",
-	PreRunE: func(cmd *cobra.Command, args []string) error {
-		if !req.wipe {
-			return errors.New("Refusing to proceed without --wipe or -f.")
-		}
-		return nil
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		if req.wipe {
-			if err := os.RemoveAll(req.outDir); err != nil {
-				panic(err)
-			}
-		}
-	},
-}
-
 func init() {
-	compileCmd.PersistentFlags().StringVar(&req.schema, "schema", "schema.firemodel", "Path to firemodel schema.")
-	rootCmd.PersistentFlags().BoolVarP(&req.wipe, "wipe", "f", false, "Whether it is ok to unlink the output directory.")
-	compileCmd.PersistentFlags().StringVarP(&req.outDir, "output-dir", "o", "./.build/firemodel", "Directory path for firemodel output.")
 	rootCmd.AddCommand(showCmd)
 	rootCmd.AddCommand(compileCmd)
-	rootCmd.AddCommand(cleanCmd)
 }
 
 func Execute() {

firemodel/cmd/show.go
@@ -8,11 +8,10 @@ import (
 
 var showCmd = &cobra.Command{
 	Use:   "show-languages",
-	Short: "Show all available languages for --out",
+	Short: "Show all available languages.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Available languages:")
 		for _, language := range firemodel.AllModelers() {
-			fmt.Println("-", language)
+			fmt.Println(language)
 		}
 	},
 }

registry.go
@@ -20,7 +20,7 @@ type Language struct {
 
 func AllModelers() (ret []string) {
 	ret = []string{}
-	for modelerName, _ := range registeredModelers {
+	for modelerName := range registeredModelers {
 		ret = append(ret, modelerName)
 	}
 	return ret

commit 77bd0b608a7b79ad1fb23492d429c553fef9cba2

Fix bug
A bug was introduced in a784f729933482d6a07d434a4885b251b984f474 that
caused code not to be written out as requested.

firemodel/cmd/compile.go
@@ -58,7 +58,7 @@ var compileCmd = &cobra.Command{
 		}
 
 		for language, outDir := range compileReq.langOutDirs {
-			if outDir != nil {
+			if outDir == nil {
 				continue
 			}
 			config.Languages = append(config.Languages, firemodel.Language{