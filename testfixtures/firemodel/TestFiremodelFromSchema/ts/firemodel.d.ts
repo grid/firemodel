865244fa3736fdbb106da81dd3b5243386442683
Zak <zak.strassberg@fileit.tax>
2018-09-25 02:16:56 -0400

    Strass/typescript defs expansion (#27)

--- testfixtures/firemodel/TestFiremodelFromSchema/ts/firemodel.d.ts
+++ testfixtures/firemodel/TestFiremodelFromSchema/ts/firemodel.d.ts
@@ -2,31 +2,28 @@
 
 import { firestore } from 'firebase';
 
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
 export interface DocumentSnapshot<DataType = firestore.DocumentData>
   extends firestore.DocumentSnapshot {
   data(options?: firestore.SnapshotOptions): DataType | undefined;
 }
-export interface QueryDocumentSnapshot<
-  DataType = firestore.DocumentData
-> extends firestore.QueryDocumentSnapshot {
+export interface QueryDocumentSnapshot<DataType = firestore.DocumentData>
+  extends firestore.QueryDocumentSnapshot {
   data(options?: firestore.SnapshotOptions): DataType | undefined;
 }
 export interface QuerySnapshot<DataType = firestore.DocumentData>
   extends firestore.QuerySnapshot {
   readonly docs: QueryDocumentSnapshot<DataType>[];
 }
-export interface DocumentSnapshotExpanded<
-  DataType = firestore.DocumentData
-> {
+export interface DocumentSnapshotExpanded<DataType = firestore.DocumentData> {
   exists: firestore.DocumentSnapshot['exists'];
   ref: firestore.DocumentSnapshot['ref'];
   id: firestore.DocumentSnapshot['id'];
   metadata: firestore.DocumentSnapshot['metadata'];
   data: DataType;
 }
-export interface QuerySnapshotExpanded<
-  DataType = firestore.DocumentData
-> {
+export interface QuerySnapshotExpanded<DataType = firestore.DocumentData> {
   metadata: {
     hasPendingWrites: firestore.QuerySnapshot['metadata']['hasPendingWrites'];
     fromCache: firestore.QuerySnapshot['metadata']['fromCache'];
@@ -37,17 +34,89 @@ export interface QuerySnapshotExpanded<
     [docId: string]: DocumentSnapshotExpanded<DataType>;
   };
 }
-export interface DocumentReference<DataType> extends firestore.DocumentReference {
-  data(options?: firestore.SnapshotOptions): DataType | undefined;
+export interface DocumentReference<DataType = firestore.DocumentData>
+  extends firestore.DocumentReference {
+  set(data: DataType, options?: firestore.SetOptions): Promise<void>;
   get(options?: firestore.GetOptions): Promise<DocumentSnapshot<DataType>>;
+  onSnapshot(observer: {
+    next?: (snapshot: DocumentSnapshot<DataType>) => void;
+    error?: (error: firestore.FirestoreError) => void;
+    complete?: () => void;
+  }): () => void;
+  onSnapshot(
+    options: firestore.SnapshotListenOptions,
+    observer: {
+      next?: (snapshot: DocumentSnapshot<DataType>) => void;
+      error?: (error: Error) => void;
+      complete?: () => void;
+    },
+  ): () => void;
+  onSnapshot(
+    onNext: (snapshot: DocumentSnapshot<DataType>) => void,
+    onError?: (error: Error) => void,
+    onCompletion?: () => void,
+  ): () => void;
+  onSnapshot(
+    options: firestore.SnapshotListenOptions,
+    onNext: (snapshot: DocumentSnapshot<DataType>) => void,
+    onError?: (error: Error) => void,
+    onCompletion?: () => void,
+  ): () => void;
 }
-export interface CollectionReference<
-  DataType = firestore.DocumentData
-> extends firestore.CollectionReference {
-  get(options?: firestore.GetOptions): Promise<QuerySnapshot<DataType>>;
+export interface CollectionReference<DataType = firestore.DocumentData>
+  extends Query<DataType>,
+    Omit<firestore.CollectionReference, keyof Query> {
+  add(data: DataType): Promise<DocumentReference>;
 }
 export interface Collection<DataType = firestore.DocumentData> {
   [id: string]: DocumentSnapshotExpanded<DataType>;
+}
+
+export interface Query<DataType = firestore.DocumentData>
+  extends firestore.Query {
+  where(
+    fieldPath: string | firestore.FieldPath,
+    opStr: firestore.WhereFilterOp,
+    value: any,
+  ): Query<DataType>;
+  orderBy(
+    fieldPath: string | firestore.FieldPath,
+    directionStr?: firestore.OrderByDirection,
+  ): Query<DataType>;
+  limit(limit: number): Query<DataType>;
+  startAt(snapshot: DocumentSnapshot): Query<DataType>;
+  startAt(...fieldValues: any[]): Query<DataType>;
+  startAfter(snapshot: DocumentSnapshot): Query<DataType>;
+  startAfter(...fieldValues: any[]): Query<DataType>;
+  endBefore(snapshot: DocumentSnapshot): Query<DataType>;
+  endBefore(...fieldValues: any[]): Query<DataType>;
+  endAt(snapshot: DocumentSnapshot): Query<DataType>;
+  endAt(...fieldValues: any[]): Query<DataType>;
+  get(options?: firestore.GetOptions): Promise<QuerySnapshot<DataType>>;
+  onSnapshot(observer: {
+    next?: (snapshot: QuerySnapshot<DataType>) => void;
+    error?: (error: Error) => void;
+    complete?: () => void;
+  }): () => void;
+  onSnapshot(
+    options: firestore.SnapshotListenOptions,
+    observer: {
+      next?: (snapshot: QuerySnapshot<DataType>) => void;
+      error?: (error: Error) => void;
+      complete?: () => void;
+    },
+  ): () => void;
+  onSnapshot(
+    onNext: (snapshot: QuerySnapshot<DataType>) => void,
+    onError?: (error: Error) => void,
+    onCompletion?: () => void,
+  ): () => void;
+  onSnapshot(
+    options: firestore.SnapshotListenOptions,
+    onNext: (snapshot: QuerySnapshot<DataType>) => void,
+    onError?: (error: Error) => void,
+    onCompletion?: () => void,
+  ): () => void;
 }
 
 // tslint:disable-next-line:no-namespace
@@ -58,7 +127,7 @@ export namespace example {
     url: URL;
     mimeType: string;
     name: string;
-	}
+  }
 
   /** TODO: Add documentation to TestDirection. */
   export enum TestDirection {
